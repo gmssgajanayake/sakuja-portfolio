290bcac7ca873d3c4810dd4d60457bbaf2674d6f
g.m.sakujashamalgajanayake <sakuja1shamal@gmail.com>
2025-02-12 11:22:22 +0530

    update 1.5

--- app/layout.js
+++ app/layout.js
@@ -66,7 +66,7 @@ export default function RootLayout({children}) {
     return (
         <html lang="en" className={"scroll-smooth"}>
         <Head>
-            <link rel="icon" href="/favicon.ico"/>
+            <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
